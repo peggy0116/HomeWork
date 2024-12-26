60a47346c58bf68ef4e2b039c558c99138988fb7
student <student@DESKTOP-F93TTDI>
2024-12-26 10:54:08 +0800

    加入專案檔案。

---
+++ 2024_12_26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_12_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Puppy DennisPuppy = new Puppy();
+            DennisPuppy.Name = "Happy";
+            DennisPuppy.Owner = "Dennis";
+            DennisPuppy.Description = "black";
+            DennisPuppy.Price = 1000;
+            DennisPuppy.Variety = "Shiba Inu";
+            DennisPuppy.PrintAllAttrib();
+
+            Puppy MarysCar = new Puppy("Happy", "Dennis", "black", 1000, "Shiba Inu");
+            MarysCar.PrintAllAttrib();
+        }
+    }
+}
---
+++ 2024_12_26/Puppy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_12_26
+{
+    internal class Puppy
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+
+        private string _Variety;
+        public string Variety
+        {
+            get { return this._Variety; }
+            set { this._Variety = value; }
+        }
+
+        public Puppy() { }
+        public Puppy(string name, string owner, string description, double price, string variety)
+        {
+            Name = name;
+            Owner = owner;
+            Description = description;
+            Price = price;
+            Variety = variety;
+        }
+
+        public void PrintAllAttrib()
+        {
+            Console.WriteLine("Name is:" + this.Name.ToString());
+            Console.WriteLine("Owener is :" + this.Owner.ToString());
+            Console.WriteLine("Description is :" + this.Description.ToString());
+            Console.WriteLine("Price is :" + this.Price.ToString());
+            Console.WriteLine("Brand is :" + this.Variety.ToString());
+        }
+    }
+}
